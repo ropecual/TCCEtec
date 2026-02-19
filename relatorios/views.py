5dab3abee587a7ef9709518b830ad1fc53abfe50
Roni Ferraresso <rope.alvarenga@gmail.com>
2026-02-19 13:48:39 -0300

    MVP: Estrutura de acesso inicial

--- relatorios/views.py
+++ relatorios/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
+from residuos.models import RegistroColeta
 
-# Create your views here.
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard.html"
+    login_url = "/admin/login/"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+        profile = user.profile
+
+        # Se for ADMIN → visão global
+        if profile.tipo == "ADMIN":
+            total_kg = RegistroColeta.objects.aggregate(
+                total=Sum('quantidade')
+            )['total'] or 0
+
+            por_tipo = (
+                RegistroColeta.objects
+                .values('tipo_residuo')
+                .annotate(total=Sum('quantidade'))
+            )
+
+        # Se for MORADOR → visão individual
+        else:
+            total_kg = RegistroColeta.objects.filter(
+                morador=user
+            ).aggregate(
+                total=Sum('quantidade')
+            )['total'] or 0
+
+            por_tipo = (
+                RegistroColeta.objects
+                .filter(morador=user)
+                .values('tipo_residuo')
+                .annotate(total=Sum('quantidade'))
+            )
+
+        context['total_kg'] = total_kg
+        context['por_tipo'] = por_tipo
+        context['tipo_usuario'] = profile.tipo
+
+        return context
